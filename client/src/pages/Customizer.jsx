e4f13ac4d7e4875443ea7388792889357f18a247
Dharmik <whatsapp83trending@gmail.com>
2023-06-06 03:24:04 -0700

    backend base url updated

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -49,7 +49,7 @@ const Customizer = () => {
     try{
         setGeneratingImg(true);
 
-        const response = await fetch('http://localhost:8000/api/v1/dalle',{
+        const response = await fetch('https://ai-image-generation-with-dalle.onrender.com/api/v1/dalle',{
             method:"POST",
             headers:{
                 'Content-Type':'application/json'
