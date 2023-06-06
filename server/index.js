b44bf1ee6e993ad77383c5162db911091e11216c
Dharmik <whatsapp83trending@gmail.com>
2023-06-06 03:16:32 -0700

    req,res interchange bug fixed

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json({limit:'50mb'}));
 app.use('/api/v1/dalle', DalleRoutes);
 
-app.get("/",(res,req)=>{
+app.get("/",(req,res)=>{
     res.status(200).json({
         message:"Hello from Dalle"
     })
